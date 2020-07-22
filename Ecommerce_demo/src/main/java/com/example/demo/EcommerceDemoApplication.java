9fcdd873becb30ece0449fe8da3478e663c704d1
Kibria92 <kibria92@gmail.com>
2020-07-22 23:11:49 +0600

    now you can add JWT

--- Ecommerce_demo/src/main/java/com/example/demo/EcommerceDemoApplication.java
+++ Ecommerce_demo/src/main/java/com/example/demo/EcommerceDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceDemoApplication {
+public class EcommerceDemoApplication {///for make a e-commerce application
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceDemoApplication.class, args);
