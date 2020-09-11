06bbe93bd1934653fc4bf458a9c4e076a6ef5633
Kibria92 <kibria92@gmail.com>
2020-09-11 19:54:45 +0600

    fix  user create time bug with address reference bug

--- Ecommerce_demo/src/main/java/com/example/demo/dto/AddressDto.java
+++ Ecommerce_demo/src/main/java/com/example/demo/dto/AddressDto.java
@@ -1,13 +1,18 @@
 package com.example.demo.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class AddressDto {
     private Long id;
     //Add validation constraint ge
-    @NotEmpty(message = "Address Name is Mandatory")
+    @NotBlank(message = "Address Name is Mandatory")
+    @Size(min = 2, max = 50, message = "Address Name is Mandatory")
+    @Length(min = 4,max = 50,message = "Address Name is Mandatory")
     private String name;
 }
